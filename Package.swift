c12b3b0d67f2ad4de13527af54ea7d4b88c3abbf
kupryk <kuprykmikita@gmail.com>
2023-12-18 13:21:20 +0100

    Add xcframework

--- Package.swift
+++ Package.swift
@@ -22,8 +22,7 @@ let package = Package(
         .target(name: "AnyCodable"),
         .binaryTarget(
             name: "libimobiledevice.c",
-            url: "https://api.github.com/repos/kupryk/HackMobile/releases/assets/141257999.zip",
-            checksum: "872cd4989573e4c56d0134e625055f813314a7ff2fbbf26bd9a64d967e0562d0"
+            path: "Resources/Automation/libimobiledevice.xcframework"
         ),
     ]
 )
